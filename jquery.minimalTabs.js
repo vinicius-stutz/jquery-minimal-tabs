9873b41f2f0fc66cad3327289361fc2eebf24617
Vinícius Stutz <vinicius-stutz@users.noreply.github.com>
2013-11-22 17:33:17 -0200

    Update jquery.minimalTabs.js
    New features

--- jquery.minimalTabs.js
+++ jquery.minimalTabs.js
@@ -4,8 +4,8 @@
 
 /**
 *
-* http://www.vinicius-stutz.com/
-* Version: 0.1 of 27 Aug 2013
+* https://github.com/blablablabla
+* Version: 0.2 of 22 Nov 2013
 * Copyright 2013 - Vinícius Stutz
 * Licensed under the MIT license
 *
@@ -16,29 +16,37 @@
 *   - Inside:
 *       $(document).ready(function(){});
 *   - Only do:
-*       $.minimalTabs();
+*       $("#ElementId").minimalTabs();
 *   - Put CSS file "jquery.minimalTabs.css" in your code;
 *   - And put this tags in your HTML code:
-*       <div id="Container">
-*           <p id="Tabs">
-*               <a href="#aba1" id="aba_1" class="abas">Aba um</a>
-*               <a href="#aba2" id="aba_2" class="abas">Aba dois</a>
-*               <a href="#aba3" id="aba_3" class="abas">Aba três</a>
-*           </p>
-*           <ul id="Content">
-*                <li id="aba1">
-*                    <div>Conteúdo da primeira aba.</div>
-*                </li>
-*                <li id="aba2">
-*                    <div>Conteúdo da segunda aba.</div>
-*                </li>
-*                <li id="aba3">
-*                    <div>Conteúdo da terceira aba.</div>
-*                </li>
-*            </ul>
-*       </div>
+        <div id="MyId" class="tabs-container">
+            <p class="tabs">
+                <a href="#Tab1" id="TabTitle1">Tab one</a>
+                <a href="#Tab2" id="TabTitle2">Tab two</a>
+                <a href="#Tab3" id="TabTitle3">Tab three</a>
+            </p>
+            <ul class="tabs-content">
+                <li id="Tab1" class="tab-content">
+                    <p>Content of first tab.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </li>
+                <li id="Tab2" class="tab-content">
+                    <p>Content of second tab.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+                </li>
+                <li id="Tab3" class="tab-content">
+                    <p>Content of third tab.</p>
+                    <ol>
+                        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                        <li>Aenean commodo ligula eget dolor.</li>
+                        <li>Aenean massa.</li>
+                    </ol>
+                    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </li>
+            </ul>
+        </div>
 *  - If you want to set a tab as active loading the page, do:
-*      $('#aba_2').click();
+*      $('#TabTitle2').click();
 *   - See "jquery.minimalTabs.css" to customize your tabs;
 *
 * Very easy to use! Too light to load (only 5 KB, this plugin and your style together)!
@@ -46,11 +54,12 @@
 **/
 
 // O código está comentado em português, mas é só porque eu sou brasileiro :-)
-$.minimalTabs = function () {
+$.fn.minimalTabs = function () {
 
     // Guarda IDs dos elementos
-    var tabs = 'p#Tabs';
-    var content = 'ul#Content';
+    var id = '#' + this.attr('id');
+    var tabs = id + ' p.tabs';
+    var content = id + ' ul.tabs-content';
 
     $(content + ' .tab-content').hide(); // Oculta todas as tabs
     $(content + ' .tab-content:first-child').show(); // Exibe a primeira aba
